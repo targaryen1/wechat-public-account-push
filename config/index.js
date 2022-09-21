7cd6e75cf127b30754bde037073dad4099e552dd
targaryen1 <110602486+targaryen1@users.noreply.github.com>
2022-09-21 20:17:39 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -60,13 +60,13 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "老婆0",
+      name: "老婆",
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "",
+      id: "gh_1487e2a64d0f",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "",
       // 所在省份
-      province: "广东",
+      province: "江苏",
       // 所在城市
       city: "肇庆",
       // 新历生日, 仅用作获取星座运势, 格式必须
